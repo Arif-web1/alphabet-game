d4de335be9ea7d71fdd75a868ce31e57bc36e425
Arif-web1 <arifhossen1110@gmail.com>
2025-01-01 20:20:18 +0600

    Test commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function play_game(params) {
+  const screen = document.getElementById("screen");
+  const landing = document.getElementById("landing_page");
+  landing.classList.add("hidden");
+  screen.classList.remove("hidden");
+  console.log(screen);
+  
+  
+}

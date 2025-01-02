f4a64d7895307e9a91e4880f7ab1afed815318cd
Arif-web1~ <arifhossen1110@gmail.com>
2025-01-02 10:46:53 +0600

    3rd commit

--- script.js
+++ script.js
@@ -3,7 +3,30 @@ function play_game(params) {
   const landing = document.getElementById("landing_page");
   landing.classList.add("hidden");
   screen.classList.remove("hidden");
-  console.log(screen);
+  const screen_input=document.getElementById('text');
+
+const random_word=random_alphabet();
+screen_input.innerText=random_word;
   
+  console.log(screen_input);
   
+}
+// random alphabet
+function random_alphabet(params) {
+    let alphabet_box=[];
+    const all_alphabet=document.querySelectorAll('kbd');
+    
+    
+    for (const element of all_alphabet) {
+        
+
+
+    alphabet_box.push(element.innerText);
+    }
+   
+
+    
+        return alphabet_box[Math.floor(Math.random() * alphabet_box.length)];
+    
+
 }

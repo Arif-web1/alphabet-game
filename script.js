853c7bbb3676bb1dca5eb6bb899b1e8135e782bc
Arif-web1~ <arifhossen1110@gmail.com>
2025-01-04 16:32:00 +0600

    design end

--- script.js
+++ script.js
@@ -1,32 +1,26 @@
 function play_game(params) {
-  const screen = document.getElementById("screen");
-  const landing = document.getElementById("landing_page");
-  landing.classList.add("hidden");
-  screen.classList.remove("hidden");
-  const screen_input=document.getElementById('text');
-
-const random_word=random_alphabet();
-screen_input.innerText=random_word;
+    const screen = document.getElementById("screen");
+    const landing = document.getElementById("landing_page");
+    landing.classList.add("hidden");
+    screen.classList.remove("hidden");
+    const screen_input = document.getElementById("text");
   
-  console.log(screen_input);
+    const random_word = random_alphabet();
+    screen_input.innerText = random_word;
+    const button_bg=document.getElementById(random_word);
+    button_bg.style.backgroundColor='orange';
+    button_bg.style.color='white';
+    console.log(screen_input);
   
-}
-// random alphabet
-function random_alphabet(params) {
-    let alphabet_box=[];
-    const all_alphabet=document.querySelectorAll('kbd');
-    
-    
+  }
+  // random alphabet
+  function random_alphabet(params) {
+    let alphabet_box = [];
+    const all_alphabet = document.querySelectorAll("kbd");
+  console.log("all_alphabet--->", all_alphabet);
+  
     for (const element of all_alphabet) {
-        
-
-
-    alphabet_box.push(element.innerText);
+      alphabet_box.push(element.innerText);
     }
-   
-
-    
-        return alphabet_box[Math.floor(Math.random() * alphabet_box.length)];
-    
-
-}
+    return alphabet_box[Math.floor(Math.random() * alphabet_box.length)];
+  }
